50206c80c0c47d1ee54b873abf07564a1ca9b0ad
Kriangkrai Ketkun <kriangkraiketkun@gmail.com>
2025-04-17 21:33:26 +0700

    feat: Add Page Not Found component with routing and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
   { path: 'add', component: UserFormComponent },
   { path: 'edit/:id', component: UserFormComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.css'
+})
+export class PageNotFoundComponent {
+  constructor(private readonly router: Router) { }
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}
